0dc2d3e5611d94fb47e54ad04f645931b2d7ea4e
shahwezdev <shahwezsiddiqui5537@gmail.com>
2026-09-23 20:10:11 +0530

    Create matrix.c
    Add matrix operations

---
+++ fundamentals/matrix.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], b[10][10], sum[10][10];
+    int rows, cols;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter first matrix:\n");
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter second matrix:\n");
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    printf("\nSum of matrices:\n");
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
